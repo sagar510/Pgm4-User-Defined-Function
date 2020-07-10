7addc8de5f19dbd2ac5641d51a11651feffcadce
Sagar Pal <57063071+sagar510@users.noreply.github.com>
2020-07-10 23:00:32 +0530

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,12 @@
+class MyProgram {
+    public static void main(String args[])
+    {
+        System.out.println("Printing from main");
+        func();
+    }
+
+    private static void func()
+    {
+        System.out.println("Printing from function");
+    }
+}
